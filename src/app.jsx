b9abdcdcdfafe2ae0782becb6b6cced31042f52c
Kaydi <@kaydancegillam@gmail.com>
2024-11-08 15:58:46 -0700

    created navbar react component in app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+  return (
+    <Navbar bg="light" expand="lg" sticky="top">
+      <Container fluid>
+        <Navbar.Brand href="https://getbootstrap.com/">
+            <h2>Temple Guesser</h2>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarSupportedContent" />
+        <Navbar.Collapse id="navbarSupportedContent">
+          <Nav className="me-auto">
+            <Nav.Link href="home.html">Home</Nav.Link>
+            <Nav.Link href="play.html">Play</Nav.Link>
+            <Nav.Link href="scoreboard.html">Scoreboard</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
 }
