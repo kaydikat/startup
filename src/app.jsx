a96d750c20182701211c7c902cf9f68c90f5caec
Kaydi <@kaydancegillam@gmail.com>
2024-11-08 16:04:43 -0700

    implemented navigation bar and footer in app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -5,20 +5,28 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 
 export default function App() {
   return (
-    <Navbar bg="light" expand="lg" sticky="top">
-      <Container fluid>
-        <Navbar.Brand href="https://getbootstrap.com/">
-            <h2>Temple Guesser</h2>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarSupportedContent" />
-        <Navbar.Collapse id="navbarSupportedContent">
-          <Nav className="me-auto">
-            <Nav.Link href="home.html">Home</Nav.Link>
-            <Nav.Link href="play.html">Play</Nav.Link>
-            <Nav.Link href="scoreboard.html">Scoreboard</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div>
+        <Navbar bg="light" expand="lg" sticky="top">
+        <Container fluid>
+            <Navbar.Brand href="https://getbootstrap.com/">
+                <h2>Temple Guesser</h2>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarSupportedContent" />
+            <Navbar.Collapse id="navbarSupportedContent">
+            <Nav className="me-auto">
+                <Nav.Link href="home.html">Home</Nav.Link>
+                <Nav.Link href="play.html">Play</Nav.Link>
+                <Nav.Link href="scoreboard.html">Scoreboard</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>
+
+        <main>App components go here</main>
+        <footer>
+        <span>Kaydance Gillam</span>
+        <a href="https://github.com/kaydikat/startup.git">GitHub</a>
+        </footer>
+    </div>
   )
 }
