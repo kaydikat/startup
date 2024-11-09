b2d803d009388931379c4935df5e8b712ab25f84
Kaydi <@kaydancegillam@gmail.com>
2024-11-09 13:07:00 -0700

    implemented scoreboard jsx

--- src/scoreboard/scoreboard.jsx
+++ src/scoreboard/scoreboard.jsx
@@ -2,11 +2,39 @@ import React from 'react';
 import './scoreboard.css';
 
 export function Scoreboard() {
+  const [scores, setScores] = React.useState([]);
+
+  // Demonstrates calling a service asynchronously so that
+  // React can properly update state objects with the results.
+  React.useEffect(() => {
+    const scoresText = localStorage.getItem('scores');
+    if (scoresText) {
+      setScores(JSON.parse(scoresText));
+    }
+  }, []);
+
+  // Demonstrates rendering an array with React
+  const scoreRows = [];
+  if (scores.length) {
+    for (const [i, score] of scores.entries()) {
+      scoreRows.push(
+        <tr key={i}>
+          <td>{i}</td>
+          <td>{score.name.split('@')[0]}</td>
+          <td>{score.score}</td>
+          <td>{score.date}</td>
+        </tr>
+      );
+    }
+  } else {
+    scoreRows.push(
+      <tr key='0'>
+        <td colSpan='4'>Be the first to score</td>
+      </tr>
+    );
+  }
   return (
     <main className="container-fluid text-center">
-      <div>
-        <h3>Your score today: <span id="score"></span>2000</h3>
-      </div>
       <section className="scoreboard">
         <header className="text-center">
           <h2>Scoreboard</h2>
@@ -20,26 +48,7 @@ export function Scoreboard() {
               <th>Date</th>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>kdg</td>
-              <td>5000</td>
-              <td>May 20, 2021</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Annie James</td>
-              <td>1350</td>
-              <td>June 2, 2021</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>Gunter Spears</td>
-              <td>1200</td>
-              <td>July 3, 2020</td>
-            </tr>
-          </tbody>
+          <tbody id='scores'>{scoreRows}</tbody>
         </table>
       </section>
     </main>
