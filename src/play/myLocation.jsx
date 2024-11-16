275ad3f1528a218bc06282b35eef2ecdd70f4289
Kaydi <@kaydancegillam@gmail.com>
2024-11-16 16:08:45 -0700

    Implemented functionality for MyLocation that fetches the geo information from IpAddress

--- src/play/myLocation.jsx
+++ src/play/myLocation.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 
-export function MyLocation(props) {
+export function MyLocation() {
   const [imageUrl, setImageUrl] = React.useState('data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=');
   const [quote, setQuote] = React.useState('Loading...');
   const [quoteAuthor, setQuoteAuthor] = React.useState('unknown');
-  const [location, setLocation] = React.useState({});
-  const [ipAddress, setIpAddress] = React.useState('');
+  const [region, setRegion] = React.useState('');
+  const [city, setCity] = React.useState('');
+  const [latitude, setLatitude] = React.useState('');
+  const [longitude, setLongitude] = React.useState('');
 
   // We only want this to render the first time the component is created and so we provide an empty dependency list.
+  const ipAddress = MyIpAddress();
   React.useEffect(() => {
-    fetch(`https://apip.cc/json/${props.ipAddress}`)
+    fetch(`https://apip.cc/json/${ipAddress}`)
       .then((response) => response.json())
       .then((data) => {
-        const containerEl = document.querySelector('#picture');
-
-        const width = containerEl.offsetWidth;
-        const height = containerEl.offsetHeight;
-        const apiUrl = `https://picsum.photos/id/${data[0].id}/${width}/${height}?grayscale`;
-        setImageUrl(apiUrl);
+        setRegion(data.region_name);
+        setCity(data.city);
+        set
       })
       .catch();
 
@@ -26,6 +26,8 @@ export function MyLocation(props) {
       .then((data) => {
         setQuote(data.quote);
         setQuoteAuthor(data.author);
+        setLatitude(data.latitude);
+        setLongitude(data.longitude);
       })
       .catch();
   }, []);
@@ -33,25 +35,7 @@ export function MyLocation(props) {
 
   
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>
-        <div id='picture' className='picture-box'>
-          <img src={imageUrl} alt='stock background' />
-        </div>
-
-        <p>Simon is a repetitive memory game where you follow the demonstrated color sequence until you make a mistake. The longer the sequence you repeat, the greater your score.</p>
-
-        <p>
-          The name Simon is a registered trademark of Milton-Bradley. Our use of the name and the game is for non-profit educational use only. No part of this code or application may be used outside
-          of that definition.
-        </p>
-
-        <div className='quote-box bg-light text-dark'>
-          <p className='quote'>{quote}</p>
-          <p className='author'>{quoteAuthor}</p>
-        </div>
-      </div>
-    </main>
+    {region}, {city}, {latitude}, {longitude}
   );
 }
 
@@ -68,10 +52,6 @@ export function MyIpAddress() {
   }, []);
 
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>
-        <p>Your IP address is {ipAddress}</p>
-      </div>
-    </main>
+    {ipAddress}
   );
 }
