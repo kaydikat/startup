a516c11efbaa4444ee7d1fbbbe304f4afa0d54b2
Kaydi <@kaydancegillam@gmail.com>
2024-11-16 14:30:36 -0700

    implemented login backend functionality

--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -10,15 +10,28 @@ export function Unauthenticated(props) {
   const navigate = useNavigate();
 
   async function loginUser() {
-    localStorage.setItem('userName', userName);
-    props.onLogin(userName);
-    navigate('/play');
+    loginOrCreate(`/api/auth/login`);
   }
 
   async function createUser() {
-    localStorage.setItem('userName', userName);
-    props.onLogin(userName);
-    navigate('/play');
+    loginOrCreate(`/api/auth/create`);
+  }
+
+  async function loginOrCreate(endpoint) {
+    const response = await fetch(endpoint, {
+      method: 'post',
+      body: JSON.stringify({ email: userName, password: password }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    if (response?.status === 200) {
+      localStorage.setItem('userName', userName);
+      props.onLogin(userName);
+    } else {
+      const body = await response.json();
+      setDisplayError(`⚠ Error: ${body.msg}`);
+    }
   }
 
   return (
