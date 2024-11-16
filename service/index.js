24b918dd549260ed8c870606f1fb688af9d16a96
Kaydi <@kaydancegillam@gmail.com>
2024-11-16 13:16:01 -0700

    added vite.confing.js file and added espress static middleware to index.js

--- service/index.js
+++ service/index.js
@@ -2,14 +2,18 @@ const express = require('express');
 const uuid = require('uuid');
 const app = express();
 
-const port = process.argv.length > 2 ? process.argv[2] : 4000;
-
-app.use(express.json());
-
 // create the memory data structures
 let users = {};
 let scores = [];
 
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.json());
+
+// Serve up the front-end static content hosting
+app.use(express.static('public'));
+
+// Router for service endpoints
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
